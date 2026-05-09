7c9ef8f5840a7ef18c667cce57f3c6abcf114983
Kamlesh142 <kamlesh65813@gmail.com>
2026-05-09 19:02:07 +0545

    Fixing errors

--- app.py
+++ app.py
@@ -127,10 +127,10 @@ def recognize():
 
 #for running the app in localhost   
 
-#if __name__ == '__main__':
-#   app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=False)
